ada24c68d316d1f520f5f1b9576a8170875c507e
Guilherme Jahnke Vieira <guiguijv@gmail.com>
2023-09-27 14:20:37 -0300

    refactor(NO-ISSUE): ( remove comments )

--- scripts/scrap_interpol.js
+++ scripts/scrap_interpol.js
@@ -1,17 +1,3 @@
-// // BASE INTERPOL SCRAPING
-// (async () => {
-//   // const keywords = ['financing of terrorism', 'Money Laundering', "Securities Fraud"];
-//   const response = await fetch(
-//     `https://ws-public.interpol.int/notices/v1/red?freeText=fraud&resultPerPage=100`,
-//   );
-//   // https://ws-public.interpol.int/notices/v1/red?freeText=fraud&resultPerPage=20&page=8
-
-//   const responseData = await response.json();
-//   // console.log(responseData);
-//   // console.log(responseData._embedded.notices.length);
-//   console.log(JSON.stringify(responseData._embedded.notices, null, 2));
-// })();
-
 class InterpolScrap {
   constructor() {
     this.baseUrl = 'https://ws-public.interpol.int/notices/v1/red?';
